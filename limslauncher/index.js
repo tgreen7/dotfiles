e9949c0cdc7e86b13d72e2d9b33d5eec34e2e67f
Taoh <tgreen@teselagen.com>
2019-12-16 12:29:44 -0800

    update lims launcher

--- limslauncher/index.js
+++ limslauncher/index.js
@@ -1,53 +1,63 @@
 const inquirer = require("inquirer");
 const shelljs = require("shelljs");
+const program = require("commander");
 
-// alias lims='osascript ~/Sites/dotfiles/scripts/lims.applescript'
-// alias limss='cd ~/Sites/lims && TG_CLIENTS=b@host yarn start-app-proxy'
-// alias limsd='cd ~/Sites/lims && TG_INSPECT_BACKEND=1 TG_CLIENTS=b@host yarn start-app-proxy'
-// alias limsr='cd ~/Sites/lims && TG_RESTART=1 TG_CLIENTS=b@host yarn start-app-proxy'
-// alias limsdr='cd ~/Sites/lims && TG_INSPECT_BACKEND=1 TG_RESTART=1 TG_CLIENTS=b@host yarn start-app-proxy'
-// alias limsrd='cd ~/Sites/lims && TG_INSPECT_BACKEND=1 TG_RESTART=1 TG_CLIENTS=b@host yarn start-app-proxy'
-// alias limsc='cd ~/Sites/lims && TG_CLIENTS=1 yarn start-build-client'
-// alias limsdeps='cd ~/Sites/lims && yarn && yarn --cwd ./e2e-tests && yarn --cwd ./server && yarn --cwd ./client  && yarn --cwd ./tg-iso-lims && yarn --cwd ./app-proxy'
+// program
+//   .option("-d, --debug", "inspect server")
+//   .option("-r, --reset", "reset database");
 
-const cmdMap = {
-  "Launch lims server and client":
-    "osascript ~/Sites/dotfiles/scripts/lims.applescript",
-  "Launch lims server":
-    "cd ~/Sites/lims && TG_RESTART=1 TG_CLIENTS=b@host yarn start-app-proxy",
-  "Launch lims server w/ debugging":
-    "cd ~/Sites/lims && TG_RESTART=1 TG_INSPECT_BACKEND=1 TG_CLIENTS=b@host yarn start-app-proxy",
-  "Launch lims server and drop and sync":
-    "cd ~/Sites/lims && TG_CLIENTS=b@host yarn start-app-proxy",
-  "Launch lims server w/ debugging and drop and sync":
-    "cd ~/Sites/lims && TG_INSPECT_BACKEND=1  TG_CLIENTS=b@host yarn start-app-proxy",
-  "Launch lims client":
-    "cd ~/Sites/lims && TG_CLIENTS=1 yarn start-build-client",
-  "Install dependencies":
-    "cd ~/Sites/lims && yarn && yarn --cwd ./e2e-tests && yarn --cwd ./server && yarn --cwd ./client  && yarn --cwd ./tg-iso-lims && yarn --cwd ./app-proxy"
-};
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "script",
-      message:
-        "What command do you want to run? (Arrow keys to select, enter to confirm)",
-      choices: [
-        "Launch lims server",
-        "Launch lims client",
-        "Launch lims server w/ debugging",
-        "Launch lims server and drop and sync",
-        "Launch lims server w/ debugging and drop and sync",
-        "Install dependencies",
-        "Launch lims server and client"
-      ]
+const cdToFolder = "cd ~/Sites/lims &&";
+const debug = "TG_INSPECT_BACKEND=1";
+const restart = "TG_RESTART=1";
+const server = "TG_CLIENTS=b@host yarn start-app-proxy";
+const client = "TG_CLIENTS=1 yarn start-build-client";
+const deps =
+  "yarn && yarn --cwd ./e2e-tests && yarn --cwd ./server && yarn --cwd ./client  && yarn --cwd ./tg-iso-lims && yarn --cwd ./app-proxy";
+
+program
+  .command("server")
+  .option("-d, --debug", "inspect server")
+  .option("-r, --reset", "reset database")
+  .description("launch lims server")
+  .action(cmdObj => {
+    let cmdToRun = server;
+
+    if (!cmdObj.reset) {
+      cmdToRun = `${restart} ${cmdToRun}`;
+    } else {
+      console.log("Resetting database");
     }
-  ])
-  .then(answers => {
-    const { script } = answers;
-    const cmd = cmdMap[script];
-    if (cmd) {
-      shelljs.exec(cmd);
+    if (cmdObj.debug) {
+      cmdToRun = `${debug} ${cmdToRun}`;
     }
+
+    cmdToRun = `${cdToFolder} ${cmdToRun}`;
+    shelljs.exec(cmdToRun);
   });
+
+program
+  .command("client")
+  .description("launch lims client")
+  .action(() => {
+    const cmdToRun = `${cdToFolder} ${client}`;
+    shelljs.exec(cmdToRun);
+  });
+
+program
+  .command("deps")
+  .description("install lims dependencies")
+  .action(() => {
+    const cmdToRun = `${cdToFolder} ${deps}`;
+    shelljs.exec(cmdToRun);
+  });
+
+program.parse(process.argv);
+
+// Check the program.args obj
+const NO_COMMAND_SPECIFIED = program.args.length === 0;
+
+// Handle it however you like
+if (NO_COMMAND_SPECIFIED) {
+  // e.g. display usage
+  program.help();
+}
--- zshcustom/sourced/aliases.zsh
+++ zshcustom/sourced/aliases.zsh
@@ -20,8 +20,6 @@ alias zshsource='source ~/.zshrc'
 alias zshtheme='$EDITOR ~/.oh-my-zsh/custom/themes/custom.zsh-theme'
 alias ohmyzsh='$EDITOR ~/Sites/dotfiles/zshcustom'
 
-alias s='$EDITOR'
-
 alias cp='cp -iv'
 alias mv='mv -iv'
 alias mkdir='mkdir -pv'
@@ -41,10 +39,6 @@ alias ucsc='sshfs tmgreen@unix.ucsc.edu:/afs/cats.ucsc.edu/users/c/tmgreen Remot
 # docker
 alias dockkill='docker-compose down'
 
-#projects
-alias website='subl ~/git/tgreen7'
-alias editweb='sshfs -p 21098 taohpjgs@server221.web-hosting.com:/ Remote'
-
 #yarn
 alias ys='yarn start'
 alias ya='yarn add'
@@ -52,7 +46,6 @@ alias yt='yarn test'
 alias yr='yarn remove'
 alias yui='yarn upgrade-interactive --latest'
 alias yb='yarn build'
-alias dys='DEBUG_DB=1 ys'
 alias yl='yarn link'
 alias yls='( ls -l node_modules ; ls -l node_modules/@* ) | grep ^l'
 
@@ -70,17 +63,17 @@ alias npms="npm i --save"
 alias npmr="npm run"
 
 # Teselagen
-tes='docker run -p 3000:3000 --privileged --rm -ti -e HIST_FILE=/root/.bash_history'
-tes+=' -v ~/.bash_history:/root/.bash_history -v ~/Sites/tesela_bashrc.txt:/root/.bashrc'
-tes+=' -v ~/Sites/j5:/usr/local/share/perl5/Bio/Tools/j5_source/ -v ~/Sites/ve:/home/teselagen/ve'
-tes+=' -v ~/Sites/appconfig.docker.json:/home/teselagen/ve/api_server_bookshelf/appconfig.docker.json'
-tes+=' dev.teselagen.com:5000/newdev /bin/bash'
-alias tes=$tes
-alias tedocker='cd ~/Sites/ve; appimagename=dev.teselagen.com:5000/newdev docker-compose run -p 3000:3000 -p 9229:9229 app'
-alias dropAndUpdate='docker exec -it ve_db /bin/bash /dockerScripts/dropAndCreateSchema.sh'
-alias randomUsers='docker exec -it ve_db /bin/bash /dockerScripts/randomUsers.sh'
-alias viewDbStartupProgress='docker logs -f ve_db'
-alias senchasass='cd ~/Sites/ve/vede; sencha ant sass'
+# tes='docker run -p 3000:3000 --privileged --rm -ti -e HIST_FILE=/root/.bash_history'
+# tes+=' -v ~/.bash_history:/root/.bash_history -v ~/Sites/tesela_bashrc.txt:/root/.bashrc'
+# tes+=' -v ~/Sites/j5:/usr/local/share/perl5/Bio/Tools/j5_source/ -v ~/Sites/ve:/home/teselagen/ve'
+# tes+=' -v ~/Sites/appconfig.docker.json:/home/teselagen/ve/api_server_bookshelf/appconfig.docker.json'
+# tes+=' dev.teselagen.com:5000/newdev /bin/bash'
+# alias tes=$tes
+# alias tedocker='cd ~/Sites/ve; appimagename=dev.teselagen.com:5000/newdev docker-compose run -p 3000:3000 -p 9229:9229 app'
+# alias dropAndUpdate='docker exec -it ve_db /bin/bash /dockerScripts/dropAndCreateSchema.sh'
+# alias randomUsers='docker exec -it ve_db /bin/bash /dockerScripts/randomUsers.sh'
+# alias viewDbStartupProgress='docker logs -f ve_db'
+# alias senchasass='cd ~/Sites/ve/vede; sencha ant sass'
 
 # git
 alias glog="git log --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short --all --since=1.days.ago --author=Taoh"
@@ -102,9 +95,6 @@ alias ggpull='git pull origin $(git_current_branch)'
 alias ggpush='git push origin $(git_current_branch)'
 alias greset='git reset --hard HEAD'
 alias gh='open $(git config remote.origin.url)'
-
-alias pushit='echo -e "\n\033[1;31m Ah push it \033[0m\n" && git push origin && echo -e "\n\033[1;32m :notes: PUSH IT REAL GOOD :notes: -🧂:hot_pepper:\033[0m\n"'
-
 alias gdelete='git branch --merged | egrep -v "(^\*|master|qa)" | xargs git branch -d'
 
 # alias lims='osascript ~/Sites/scripts/launch-lims.scpt'
@@ -112,7 +102,6 @@ alias gdelete='git branch --merged | egrep -v "(^\*|master|qa)" | xargs git bran
 # alias limsserver='cd ~/Sites/lims/server && ys'
 # alias limsdev='ssh -i "~/.ssh/teselagen_dev.pem" ubuntu@ec2-52-9-77-80.us-west-1.compute.amazonaws.com'
 
-alias qa='gco qa && ggpull && git merge master && ggpush && gco master'
 # sudo -s
 # docker exec -ti lims_app bash
 
