a4bbd28f4bf9db5e0572a1dc75b5566317c2c52e
Taoh <tgreen@teselagen.com>
2019-12-19 10:39:08 -0800

    Update lims launcher with new option

--- limslauncher/index.js
+++ limslauncher/index.js
@@ -4,19 +4,21 @@ const program = require("commander");
 const cdToFolder = "cd ~/Sites/lims";
 const debug = "TG_INSPECT_BACKEND=1";
 const restart = "TG_RESTART=1";
+const refreshSchema = "refreshSchema=1";
+
+const commonDeps =
+  "yarn && yarn --cwd ./e2e-tests && yarn --cwd ./server && yarn --cwd ./app-proxy";
 
 let moduleName = "lims";
 let server = "TG_CLIENTS=b@host yarn start-app-proxy";
 let client = "TG_CLIENTS=1 yarn start-build-client";
-let deps =
-  "yarn && yarn --cwd ./e2e-tests && yarn --cwd ./server && yarn --cwd ./client  && yarn --cwd ./tg-iso-lims && yarn --cwd ./app-proxy";
+let deps = `${commonDeps} && yarn --cwd ./client && yarn --cwd ./tg-iso-lims`;
 
 if (process.env.HDE) {
   moduleName = "hde";
   server = "TG_CLIENTS=d@host yarn start-app-proxy";
   client = "TG_CLIENTS=1 yarn start-design-client";
-  deps =
-    "yarn && yarn --cwd ./e2e-tests && yarn --cwd ./server && yarn --cwd ./client-desgin  && yarn --cwd ./tg-iso-design && yarn --cwd ./app-proxy";
+  deps = `${commonDeps} && yarn --cwd ./client-design  && yarn --cwd ./tg-iso-design`;
 }
 
 program.name(moduleName);
@@ -25,15 +27,17 @@ program
   .command("server")
   .option("-d, --debug", "inspect server")
   .option("-r, --reset", "reset database")
+  .option("-k, --kill", "reset database and containers")
   .description(`launch ${moduleName} server`)
   .action(cmdObj => {
-    let cmdToRun = server;
+    let cmdToRun = `${restart} ${server}`;
 
-    if (!cmdObj.reset) {
-      cmdToRun = `${restart} ${cmdToRun}`;
-    } else {
-      console.log("Resetting database");
+    if (cmdObj.reset) {
+      cmdToRun = `${restart} ${refreshSchema} ${server}`;
+    } else if (cmdObj.kill) {
+      cmdToRun = server;
     }
+
     if (cmdObj.debug) {
       cmdToRun = `${debug} ${cmdToRun}`;
     }
